a9d5fa2e1fba92e221aaf1e85226c003644173a4
oCyberBR2k18 <joseeduardoampla@gmail.com>
2019-08-29 17:23:18 -0300

    Added some configurations

--- src/main/java/net/snuckdev/snuckmissions/commands/MissionsCommand.java
+++ src/main/java/net/snuckdev/snuckmissions/commands/MissionsCommand.java
@@ -1,5 +1,6 @@
 package net.snuckdev.snuckmissions.commands;
 
+import net.snuckdev.snuckmissions.Main;
 import net.snuckdev.snuckmissions.items.ItemBuilder;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -8,8 +9,14 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MissionsCommand implements CommandExecutor {
 
@@ -28,16 +35,42 @@ public class MissionsCommand implements CommandExecutor {
             return true;
         }
 
-        Inventory inv = Bukkit.getServer().createInventory(null, 9*3, "Menu de missões");
-        inv.setItem(10, new ItemBuilder(Material.DIAMOND_PICKAXE).setDisplayName("§aBlocos quebrados").setLore(Arrays.asList(
-                "§7Veja aqui o progresso das suas conquistas",
-                "§7de quebrar blocos."
-        )).build());
+        //region brokenblocks
+        int brokenBlocksPosition = Main.plugin.getConfig().getInt("menu-principal.blocos-quebrados.posicao");
+        String brokenBlocksMaterial = Main.plugin.getConfig().getString("menu-principal.blocos-quebrados.item");
+        String brokenBlocksName = Main.plugin.getConfig().getString("menu-principal.blocos-quebrados.name");
 
-        inv.setItem(12, new ItemBuilder(Material.DIRT).setDisplayName("§aBlocos colocados").setLore(Arrays.asList(
-                "§7Veja aqui o progresso das suas conquistas",
-                "§7de colocar blocos."
-        )).build());
+        Inventory inv = Bukkit.getServer().createInventory(null, 9*3, "Menu de missões");
+        ItemStack brokenBlocksPickaxe = new ItemBuilder(Material.valueOf(brokenBlocksMaterial)).
+                setDisplayName(brokenBlocksName).build();
+        ItemMeta brokenBlocksMeta = brokenBlocksPickaxe.getItemMeta();
+        List<String> lore = new ArrayList<>();
+        for(String s : Main.plugin.getConfig().getStringList("menu-principal.blocos-quebrados.lore")) {
+            lore.add(s.replace("&", "§"));
+        }
+        brokenBlocksMeta.setLore(lore);
+        brokenBlocksMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        brokenBlocksPickaxe.setItemMeta(brokenBlocksMeta);
+        inv.setItem(brokenBlocksPosition, brokenBlocksPickaxe);
+        //endregion
+
+        //region placedblocks
+        int placedBlocksPosition = Main.plugin.getConfig().getInt("menu-principal.blocos-colocados.posicao");
+        String placedBlocksMaterial = Main.plugin.getConfig().getString("menu-principal.blocos-colocados.item");
+        String placedBlocksName = Main.plugin.getConfig().getString("menu-principal.blocos-colocados.name");
+
+        ItemStack placedBlocksDirt = new ItemBuilder(Material.valueOf(placedBlocksMaterial))
+                .setDisplayName(placedBlocksName)
+                .build();
+        ItemMeta placedBlocksMeta = placedBlocksDirt.getItemMeta();
+        List<String> placedBlocksLore = new ArrayList<>();
+        for(String s : Main.plugin.getConfig().getStringList("menu-principal.blocos-colocados.lore")) {
+            placedBlocksLore.add(s.replace("&", "§"));
+        }
+        placedBlocksMeta.setLore(placedBlocksLore);
+        placedBlocksDirt.setItemMeta(placedBlocksMeta);
+        inv.setItem(placedBlocksPosition, placedBlocksDirt);
+        //endregion
 
         inv.setItem(14, new ItemBuilder(Material.DIAMOND_SWORD).setDisplayName("§aJogadores mortos").setLore(Arrays.asList(
                 "§7Veja aqui o progresso das suas conquistas",
